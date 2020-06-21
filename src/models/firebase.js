7dd674d2ee2f295fe64bb19ef9ca2a1db12c7318
John Lin <johnlin72499@gmail.com>
2020-06-20 22:29:55 -0400

    separate dev firebase and prod

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -1,7 +1,13 @@
 import firebase from 'firebase';
 import 'firebase/firestore'
+
 import firebaseConfig from '../firebase.config';
+import firebaseConfigDev from '../firebase.dev.config.js';
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (process.env.NODE_ENV === 'production') {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.initializeApp(firebaseConfigDev);
+}
 firebase.analytics();
