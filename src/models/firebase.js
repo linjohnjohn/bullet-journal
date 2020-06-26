3ea65a8adc7e4e9540f238f0fadcc234107c1b02
John Lin <johnlin72499@gmail.com>
2020-06-26 11:34:34 -0400

    enable deploy to staging server

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -5,7 +5,7 @@ import firebaseConfig from '../firebase.config';
 import firebaseConfigDev from '../firebase.dev.config.js';
 
 // Initialize Firebase
-if (process.env.NODE_ENV === 'production') {
+if (process.env.FIREBASE_ENV === 'production') {
     firebase.initializeApp(firebaseConfig);
 } else {
     firebase.initializeApp(firebaseConfigDev);
